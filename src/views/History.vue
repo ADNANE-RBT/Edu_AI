99a3ccb839c4ddb9883e726f31eaddb18878bb89
ADNANE-RBT <92606656+ADNANE-RBT@users.noreply.github.com>
2024-05-29 03:50:07 +0100

    first-commit

---
+++ src/views/History.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <div>
+        <div class="row">
+      <div class="col-md-7">
+        <billing-card />
+      </div>
+      <div class="col-md-5">
+        <invoice-card class="mt-4" />
+      </div>
+    </div>
+    </div>
+</template>
+<script setup>
+// import MasterCard from "@/examples/Cards/MasterCard.vue";
+// import DefaultInfoCard from "@/examples/Cards/DefaultInfoCard.vue";
+// import PaymentCard from "./components/PaymentCard.vue";
+import InvoiceCard from "./components/InvoiceCard.vue";
+import BillingCard from "./components/BillingCard.vue";
+// import TransactionCard from "./components/TransactionCard.vue";
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/InsertDegree.vue
@@ -0,0 +1,297 @@
+
+<script setup>
+import ArgonInput from "@/components/ArgonInput.vue";
+import ArgonButton from "@/components/ArgonButton.vue";
+
+
+</script>
+
+
+<template lang="">
+    <div class="w-full">
+        <div class="col-md-8 mx-auto">
+          <div class="card">
+            <div class="card-header pb-0">
+              <div class="d-flex align-items-center">
+                <p class="mb-0">Insert Degree</p>
+                <argon-button color="success" size="sm" class="ms-auto"
+                  >Submit</argon-button
+                >
+              </div>
+            </div>
+            <div class="card-body">
+              <p class="text-uppercase text-sm">Master Information</p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >name</label
+                  >
+                  <argon-input type="text" value="lucky.jesse" />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >University name</label
+                  >
+                  <argon-input type="text" value="lucky.jesse" />
+                </div>
+        
+              </div>
+              <div class="row">
+                <div class="col-md-10">
+                  <label for="example-text-input" class="form-control-label"
+                    >Description</label
+                  >
+                  <argon-input type="text" value="lucky.jesse" />
+                </div>
+           
+        
+              </div>
+
+
+              <hr class="horizontal dark" />
+              <p class="text-uppercase text-sm">Semester 1 modules </p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 111</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 112</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 113</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 114</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 115</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 116</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+               
+              </div>
+              <hr class="horizontal dark" />
+              <p class="text-uppercase text-sm">Semester 2 </p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 121</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 122</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 123</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 124</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 125</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 126</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+               
+              </div>
+              <hr class="horizontal dark" />
+              <p class="text-uppercase text-sm">Semester 3 </p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 211</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 212</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 213</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 214</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 215</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 216</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+               
+              </div>
+
+
+              <hr class="horizontal dark" />
+              <p class="text-uppercase text-sm">Semester 4 </p>
+              <div class="row">
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 221</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 222</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 223</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 224</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 225</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+                <div class="col-md-6">
+                  <label for="example-text-input" class="form-control-label"
+                    >module 226</label
+                  >
+                  <argon-input
+                    type="text"
+                    value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
+                  />
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
